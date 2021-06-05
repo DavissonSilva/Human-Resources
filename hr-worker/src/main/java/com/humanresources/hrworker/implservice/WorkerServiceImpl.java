74fe2a897f4332320b2387dce9d244ca67f3323e
dcardosi <davisson.cardoso.silva@everis.com>
2021-06-05 12:51:47 -0300

    configurando o eureka client

--- hr-worker/src/main/java/com/humanresources/hrworker/implservice/WorkerServiceImpl.java
+++ hr-worker/src/main/java/com/humanresources/hrworker/implservice/WorkerServiceImpl.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.humanresources.hrworker.entity.Worker;
